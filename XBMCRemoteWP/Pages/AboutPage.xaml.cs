d5b10040a4253c72f55ebbe0166e6687068fa549
Akshay Zade <akshay2000@hotmail.com>
2013-09-02 22:03:51 +0530

    Completed about page
    Done with basic AboutPage.xaml. Added FeedbackPage.xaml.

--- XBMCRemoteWP/Pages/AboutPage.xaml.cs
+++ XBMCRemoteWP/Pages/AboutPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace XBMCRemoteWP.Pages
 {
@@ -15,6 +16,24 @@ namespace XBMCRemoteWP.Pages
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private void DevProfile_Click(object sender, RoutedEventArgs e)
+        {
+            MarketplaceSearchTask marketplaceSearchTask = new MarketplaceSearchTask();
+            marketplaceSearchTask.SearchTerms = "akshay2000";
+            marketplaceSearchTask.Show();
+        }
+
+        private void Email_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/FeedbackPage.xaml", UriKind.Relative));
+        }
+
+        private void RateApp_Click(object sender, RoutedEventArgs e)
+        {
+            MarketplaceReviewTask RateApp = new MarketplaceReviewTask();
+            RateApp.Show();
         }
     }
 }
---
+++ XBMCRemoteWP/Pages/FeedbackPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace XBMCRemoteWP.Pages
+{
+    public partial class FeedbackPage : PhoneApplicationPage
+    {
+        public FeedbackPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
